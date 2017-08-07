83f885376b338269c84991c5e05acd076db6e3dc
Şahin Olut <sahinolut@outlook.com>
2017-08-07 05:39:41 +0300

    Create utils.py

---
+++ Generative_Adversarial_Networks/InfoGAN_mnist/utils.py
@@ -0,0 +1,91 @@
+#Taken from CS231N class.
+def show_images(images):
+    images = np.reshape(images, [images.shape[0], -1])  # images reshape to (batch_size, D)
+    sqrtn = int(np.ceil(np.sqrt(images.shape[0])))
+    sqrtimg = int(np.ceil(np.sqrt(images.shape[1])))
+
+    fig = plt.figure(figsize=(sqrtn, sqrtn))
+    gs = gridspec.GridSpec(sqrtn, sqrtn)
+    gs.update(wspace=0.05, hspace=0.05)
+
+    for i, img in enumerate(images):
+        ax = plt.subplot(gs[i])
+        plt.axis('off')
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.set_aspect('equal')
+        plt.imshow(img.reshape([sqrtimg,sqrtimg]))
+    return 
+
+def preprocess_img(x):
+    return 2 * x - 1.0
+
+def deprocess_img(x):
+    return (x + 1.0) / 2.0
+
+def rel_error(x,y):
+    return np.max(np.abs(x - y) / (np.maximum(1e-8, np.abs(x) + np.abs(y))))
+
+def count_params(model):
+    """Count the number of parameters in the current TensorFlow graph """
+    param_count = np.sum([np.prod(p.size()) for p in model.parameters()])
+    return param_count
+
+
+
+class ChunkSampler(sampler.Sampler):
+    """Samples elements sequentially from some offset. 
+    Arguments:
+        num_samples: # of desired datapoints
+        start: offset where we should start selecting from
+    """
+    def __init__(self, num_samples, start=0):
+        self.num_samples = num_samples
+        self.start = start
+
+    def __iter__(self):
+        return iter(range(self.start, self.start + self.num_samples))
+
+    def __len__(self):
+        return self.num_samples
+
+
+
+def sample_noise(batch_size, dim):
+    
+    #Generates torch.Tensor [batch_size, dim] with variables between [-1,1]
+    
+    out = torch.randn(batch_size,dim)
+    out = torch.tanh(out)
+    return out
+ 
+class Flatten(nn.Module):
+    def forward(self, x):
+        N, C, H, W = x.size() # read in N, C, H, W
+        return x.view(N, -1)  # "flatten" the C * H * W values into a single vector per image
+    
+class Unflatten(nn.Module):
+    """
+    An Unflatten module receives an input of shape (N, C*H*W) and reshapes it
+    to produce an output of shape (N, C, H, W).
+    """
+    def __init__(self, N=-1, C=128, H=7, W=7):
+        super(Unflatten, self).__init__()
+        self.N = N
+        self.C = C
+        self.H = H
+        self.W = W
+    def forward(self, x):
+        return x.view(self.N, self.C, self.H, self.W)
+
+def initialize_weights(m):
+    if isinstance(m, nn.Linear) or isinstance(m, nn.ConvTranspose2d):
+        init.xavier_uniform(m.weight.data)
+
+
+
+
+
+
+
+
